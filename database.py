4d1ca5ffe38f849ca8cd2e58ec67187a02e438a4
Peerapat Sungkasem <gigapixel7@gmail.com>
2015-04-17 16:42:41 +0700

    done sql generation script

--- database.py
+++ database.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 import pymysql
 
 class database:
@@ -12,14 +14,26 @@ class database:
             passwd='1q2w3e4r',
             db='ops'
         )
-        cls.cursor = connection.cursor()
+        cls.cursor = cls.connection.cursor()
 
     @classmethod
     def close_connection(cls):
         cls.connection.close()
 
     @classmethod
-    def get_mat_from_sap_matcode(cls, sap_mat_code='1'):
+    def get_mat_from_sap_matcode(cls, sap_mat_code):
         sql = "SELECT * FROM materials WHERE sap_mat_code='%s'" % sap_mat_code
+        cls.cursor.execute(sql)
+        return cls.cursor.fetchone()
+
+    @classmethod
+    def get_supplier_from_supplier_code(cls, supplier_code):
+        sql = "SELECT * FROM suppliers WHERE supplier_code='%s'" % supplier_code
+        cls.cursor.execute(sql)
+        return cls.cursor.fetchone()
+
+    @classmethod
+    def get_location_from_location_id(cls, location_id):
+        sql = "SELECT * FROM locations WHERE location_id='%s'" % location_id
         cls.cursor.execute(sql)
         return cls.cursor.fetchone()
---
+++ yamato_excel.py
@@ -0,0 +1,144 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import sys
+import textwrap
+import openpyxl
+
+from database import *
+from yamato_utils import *
+
+
+class yamato_excel:
+    current_uid = 1
+    worksheet = None
+    items = []
+    lost_locations = {}
+    lost_materials = {}
+    columns = {
+        'date': 'A',
+        'warehouse_code': 'B',
+        'item_code': 'C',
+        'item_name': 'D',
+        'item_color': 'E',
+        'location_no': 'F',
+        'qty_on_hand': 'G'
+    }
+    status = {
+        'GOOD': 1,
+        'B2B': 2,
+        'DEFECT': 3,
+        'GRADING': 3,
+        'DAMAGE': 4
+    }
+
+    @classmethod
+    def load_data(cls, filename, sheetname='Inventory for check stock'):
+        workbook = openpyxl.load_workbook(filename)
+        cls.worksheet = workbook[sheetname]
+
+        for row in range(2, len(cls.worksheet.rows) + 1):
+            data = {
+                'date': cls.get_cell_value(row, 'date'),
+                'warehouse_code': cls.get_cell_value(row, 'warehouse_code'),
+                'item_code': cls.get_cell_value(row, 'item_code'),
+                'item_name': cls.get_cell_value(row, 'item_name'),
+                'item_color': cls.get_cell_value(row, 'item_color'),
+                'location_no': cls.get_cell_value(row, 'location_no'),
+                'qty_on_hand': cls.get_cell_value(row, 'qty_on_hand')
+            }
+            cls.items.append(data)
+
+    @classmethod
+    def get_cell_value(cls, row, col):
+        return cls.worksheet[cls.columns[col] + str(row)].value
+
+    @classmethod
+    def generate_sql_file(cls, filename='yamato.sql'):
+        sqlfile = open(filename, 'w')
+        uid = 'TH011500000'
+
+        for item in cls.items:
+            location = database.get_location_from_location_id(item['location_no'])
+            material = database.get_mat_from_sap_matcode(item['item_code'])
+
+            if location == None:
+                cls.lost_locations[item['location_no']] = "Not found location: %s" % item['location_no']
+                continue
+
+            if material == None:
+                cls.lost_materials[item['item_code']] = "Not found material: %s" % item['item_code']
+                continue
+
+            supplier = database.get_supplier_from_supplier_code(material[2])
+
+            qty = int(item['qty_on_hand'])
+            for i in range(0, qty):
+                uid = get_next_uid(uid)
+
+                sql_item = cls.build_item_statement(uid, item, material, supplier)
+                sql_history = cls.build_item_history_statement(uid, item)
+
+                sqlfile.write(textwrap.dedent(sql_item.encode('utf-8')))
+                sqlfile.write(textwrap.dedent(sql_history.encode('utf-8')))
+
+        sqlfile.close()
+
+    @classmethod
+    def build_item_statement(cls, uid, item, material, supplier):
+        supplier_code = supplier[0]
+        company_name = supplier[2]
+
+        sku_mat_code = material[0]
+        supplier_sku = material[5]
+
+        item_name = item['item_name'].replace("'", "\\'")
+        location_id = item['location_no']
+        status_id = cls.status[item['warehouse_code']]
+
+        sql = """
+            INSERT INTO items (uid, sku_mat_code, item_name, location_id, status_id, updated_by, company_name, supplier_code, supplier_sku)
+            VALUES ('%s', '%s', '%s', '%s', %s, '%s', '%s', '%s', '%s');
+        """ % (uid, sku_mat_code, item_name, location_id, status_id, 'Yamato', company_name, supplier_code, supplier_sku)
+        return sql
+
+    @classmethod
+    def build_item_history_statement(cls, uid, item):
+        status_id = cls.status[item['warehouse_code']]
+
+        sql = """
+            INSERT INTO item_history (uid, status_id, created_by)
+            VALUES ('%s', %s, '%s');
+        """ % (uid, status_id, 'Yamato')
+        return sql
+
+    @classmethod
+    def log_error_items(cls):
+        if cls.lost_locations == {} and cls.lost_materials == {}:
+            print "Not found any error, your data is ready to import."
+            return True
+        else:
+            print "Data errors:"
+
+            for location in cls.lost_locations.keys():
+                print cls.lost_locations[location]
+            for material in cls.lost_materials.keys():
+                print cls.lost_materials[material]
+
+            return False
+
+
+if __name__ == "__main__":
+    print sys.argv[1]
+
+    if not len(sys.argv):
+        print "Please specify excel file."
+    else:
+        database.create_connection()
+        yamato_excel.load_data(sys.argv[1])
+
+        print "Generating sql file please wait ..."
+        yamato_excel.generate_sql_file('yamato.sql')
+        print "Data generate completed."
+
+        yamato_excel.log_error_items()
